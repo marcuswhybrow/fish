4877cbaf7b5af2167f4c05b014e790a54923dca7
Marcus Whybrow <marcus@whybrow.uk>
2023-11-30 23:00:30 +0000

    Remove tmux input: circular dep. in other flake

--- config.nix
+++ config.nix
@@ -1,6 +1,5 @@
 { pkgs, inputs, ... }: let 
   hyprland = "${pkgs.hyprland}/bin/Hyprland";
-  tmux = "${inputs.tmux.packages.x86_64-linux.tmux}/bin/tmux";
   neovim = "${inputs.neovim.packages.x86_64-linux.nvim}/bin/nvim";
   git = "${inputs.git.packages.x86_64-linux.git}/bin/git";
   starship = "${inputs.starship.packages.x86_64-linux.starship}/bin/starship";
@@ -13,8 +12,8 @@ in ''
   end
 
   if status is-interactive
-    abbr --add n "${tmux} new -A -s nixos -c ~/.nixos"
-    abbr --add c "${tmux} new -A -s config -c ~/.config"
+    abbr --add n "tmux new -A -s nixos -c ~/.nixos"
+    abbr --add c "tmux new -A -s config -c ~/.config"
     abbr --add r work_on_repository
 
     abbr --add t ${neovim} ~/obsidian/Timeline/$(date +%Y-%m-%d).md
