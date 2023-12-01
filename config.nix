3d0d823b7d6f48ba75991d8a3d7a6b3204d3b92c
Marcus Whybrow <marcus@whybrow.uk>
2023-12-01 12:04:59 +0000

    Remove hyprland login: see marcuswhybrow/hyprland

--- config.nix
+++ config.nix
@@ -1,16 +1,10 @@
 { pkgs, inputs, ... }: let 
-  hyprland = "${pkgs.hyprland}/bin/Hyprland";
+  hyprland = "${inputs.hyprland.packages.x86_64-linux.hyprland}/bin/Hyprland";
   neovim = "${inputs.neovim.packages.x86_64-linux.nvim}/bin/nvim";
   git = "${inputs.git.packages.x86_64-linux.git}/bin/git";
   starship = "${inputs.starship.packages.x86_64-linux.starship}/bin/starship";
   direnv = "${pkgs.direnv}/bin/direnv";
 in ''
-  if status is-login
-    if [ (hostname) = "marcus-laptop" ]
-      ${hyprland}
-    end
-  end
-
   if status is-interactive
     abbr --add n "tmux new -A -s nixos -c ~/.nixos"
     abbr --add c "tmux new -A -s config -c ~/.config"
