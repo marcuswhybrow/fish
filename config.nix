4e6c3e157010f30ac925fedcb21eefa37c664d6f
Marcus Whybrow <marcus@whybrow.uk>
2024-05-03 12:13:09 +0100

    Remove starship init (now in starship package)

--- config.nix
+++ config.nix
@@ -1,12 +1,9 @@
 { pkgs, inputs, mwpkgs, ... }: let 
-  starship = "${mwpkgs.starship}/bin/starship";
   direnv = "${pkgs.direnv}/bin/direnv";
 in ''
   if status is-interactive
     abbr --add osswitch sudo nixos-rebuild switch
     abbr --add ostest sudo nixos-rebuild test
-
-    ${starship} init fish | source
     ${direnv} hook fish | source
   end
 ''
