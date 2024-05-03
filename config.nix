a44f67e1e2e0d5ad5d0ad2df360fa14d24fb0496
Marcus Whybrow <marcus@whybrow.uk>
2024-05-03 11:50:57 +0100

    Remove neovim/obsidian abbreviations

--- config.nix
+++ config.nix
@@ -1,5 +1,4 @@
 { pkgs, inputs, mwpkgs, ... }: let 
-  neovim = "${mwpkgs.neovim}/bin/vim";
   starship = "${mwpkgs.starship}/bin/starship";
   direnv = "${pkgs.direnv}/bin/direnv";
 in ''
@@ -7,9 +6,6 @@ in ''
     abbr --add n "tmux new -A -s nixos -c ~/.nixos"
     abbr --add c "tmux new -A -s config -c ~/.config"
     abbr --add r work_on_repository
-
-    abbr --add t ${neovim} ~/obsidian/Timeline/$(date +%Y-%m-%d).md
-    abbr --add y ${neovim} ~/obsidian/Timeline/$(date +%Y-%m-%d --date yesterday).md
 
     abbr --add osswitch sudo nixos-rebuild switch
     abbr --add ostest sudo nixos-rebuild test
