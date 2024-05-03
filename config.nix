abf6b21cc6dec525bad79e7648c3cdd0412856d0
Marcus Whybrow <marcus@whybrow.uk>
2024-05-03 11:57:59 +0100

    Remove tmux abbreviations (now in tmux package)

--- config.nix
+++ config.nix
@@ -3,10 +3,6 @@
   direnv = "${pkgs.direnv}/bin/direnv";
 in ''
   if status is-interactive
-    abbr --add n "tmux new -A -s nixos -c ~/.nixos"
-    abbr --add c "tmux new -A -s config -c ~/.config"
-    abbr --add r work_on_repository
-
     abbr --add osswitch sudo nixos-rebuild switch
     abbr --add ostest sudo nixos-rebuild test
 
