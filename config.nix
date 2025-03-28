303257c5fd85f2e00765cc647af09f3dab3a0ff4
Marcus Whybrow <marcus@whybrow.uk>
2025-03-28 18:10:04 +0000

    Simplify config & fish function code

--- config.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, inputs, ... }: let 
-  direnv = "${pkgs.direnv}/bin/direnv";
-in ''
-  if status is-interactive
-    abbr --add osswitch sudo nixos-rebuild switch
-    abbr --add ostest sudo nixos-rebuild test
-    ${direnv} hook fish | source
-  end
-''
--- flake.nix
+++ flake.nix
@@ -9,27 +9,33 @@
   outputs = inputs: let
     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
     flakeUpdates = inputs.flake-updates.packages.x86_64-linux.flake-updates;
-    configText = import ./config.nix { inherit pkgs inputs; };
-    config = pkgs.writeTextDir "share/fish/vendor_conf.d/config.fish" configText;
-    functions = {
-      fish_greeting = ''
+    direnv = "${pkgs.direnv}/bin/direnv";
+
+    config = pkgs.writeTextDir "share/fish/vendor_conf.d/config.fish" /* fish */ ''
+      if status is-interactive
+        abbr --add osswitch sudo nixos-rebuild switch
+        abbr --add ostest sudo nixos-rebuild test
+        ${direnv} hook fish | source
+      end
+    '';
+
+    fish_greeting = pkgs.writeTextDir "share/fish/vendor_functions.d/fish_greeting.fish" /* fish */ ''
+      function fish_greeting
         echo (whoami) @ (hostname)
         set updates (${flakeUpdates}/bin/flake-updates --flake ~/Repos/nixos --output '%s' --defer)
         if test -n "$updates"
-          echo "$updates updates available"
+          echo "$updates updates available (2)"
         end
-      '';
-    };
-    mkFunctionPkg = name: def: (pkgs.writeTextDir "share/fish/vendor_functions.d/${name}.fish" ''
-      function ${name}
-        ${def}
       end
-    '');
-    functionPkgs = pkgs.lib.mapAttrsToList mkFunctionPkg functions;
+    '';
   in {
     packages.x86_64-linux.fish = pkgs.symlinkJoin {
       name = "fish";
-      paths = [ pkgs.fish config ] ++ functionPkgs;
+      paths = [ 
+        pkgs.fish 
+        config 
+        fish_greeting 
+      ];
     };
     packages.x86_64-linux.default = inputs.self.packages.x86_64-linux.fish;
   };
